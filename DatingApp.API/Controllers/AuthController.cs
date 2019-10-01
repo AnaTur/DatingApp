851820e788bff7bc888176171c10ebe689aef0f5
Ana Tur <anatur@Anas-MacBook-Pro.local>
2019-10-01 17:37:16 +0800

    End of Section 11

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -16,13 +17,15 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private IAuthRepository _repo;
-        private IConfiguration _config;
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
 
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
         {
             _repo = repo;
             _config = config;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -70,8 +73,12 @@ namespace DatingApp.API.Controllers
 
                 var token = tokenHandler.CreateToken(tokenDescriptor);
 
-                return Ok(new {
-                    token = tokenHandler.WriteToken(token)
+                var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+                return Ok(new
+                {
+                    token = tokenHandler.WriteToken(token),
+                    user
                 });
         }
     }
